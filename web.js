8f86c58ce48a54108c5f554f32414304b2f81cf7
smarrapu <smarrapu@yahoo.com>
2013-07-11 06:36:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
