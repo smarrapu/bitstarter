7a17f9c3d5dc78d50c39a4de33e06ce7fe7ed451
smarrapu <smarrapu@yahoo.com>
2013-07-17 03:45:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8"));
 });
 
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
