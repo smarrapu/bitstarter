8009ccf7b2411ad0379fe9c64632a5050a6bed3f
smarrapu <smarrapu@yahoo.com>
2013-07-18 02:58:39 +0000

    Updated graders.js

--- grader.js
+++ grader.js
@@ -65,6 +65,7 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .potion('-u, --url','Path to external url')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
